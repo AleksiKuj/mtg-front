1a2ebade38640d360f7bbb1cff031358af01b5ff
AleksiKuj <kujala.aleksi99@gmail.com>
2024-02-05 15:28:28 +0200

    api url fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,18 +1,9 @@
 import axiosInstance from "utils/axiosConfig"
-import axios from "axios"
 
 //generic get call
 export const fetchData = async (url: string) => {
   try {
     const response = await axiosInstance.get(url)
-    return response.data
-  } catch (e) {
-    console.error(e)
-  }
-}
-export const fetchExternalData = async (url: string) => {
-  try {
-    const response = await axios.get(url)
     return response.data
   } catch (e) {
     console.error(e)
